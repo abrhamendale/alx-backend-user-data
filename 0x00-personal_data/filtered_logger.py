343af500b0ae76d29da4e233bd7705187b66e34f
Abrham Endale <abrhamendale9@gmail.com>
2023-05-25 13:01:28 +0000

    Added task_0.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,9 +4,33 @@
 
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> List[str]:
-    message = re.sub("password=[\d\w]*;", "password="+redaction+";", message)
-    message = re.sub("date_of_birth=[0-9]{2}\/[0-9]{2}\/[0-9]{4};", "date_of_birth="+redaction+";", message)
+    for i in fields:
+        obf = i + "=[\d\w\S][^;]*;"
+        fld = i + "=" + redaction + ";"
+        message = re.sub(obf, fld, message)
     return (message)
+"""
+message = re.sub(p, "password="+redaction+";", message)
+#message = re.sub("password=[\d\w]*;", "password="+redaction+";", message)
+"""
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+    FIELDS = ""
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.FIELDS = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        print(record)
+        
