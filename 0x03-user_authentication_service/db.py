9d43f54a7f462a2cb8c4b10e050bb7074d54b3de
Abrham Endale <tmailae2020@gmail.com>
2023-06-06 23:05:58 +0300

    A correction.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -19,7 +19,7 @@ class DB:
     DB class
     """
 
-    def __init__(self) -> None:
+    def __init__(self):
         """
         Initialize a new DB instance
         """
@@ -38,7 +38,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, em, h_p) -> TypeVar(User):
+    def add_user(self, em: str, h_p: str) -> TypeVar(User):
         """
         A function to add a user.
         """
