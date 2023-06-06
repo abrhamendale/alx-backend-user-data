afe3b2d1f449b5891ac84283f7f6a610639cb072
Abrham Endale <tmailae2020@gmail.com>
2023-06-06 19:37:58 +0300

    A fix for task_0.

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,6 +10,9 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 from sqlalchemy import Column, Integer, String
 class User(Base):
+    """
+    A user class for users.
+    """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -19,4 +22,8 @@ class User(Base):
     reset_token = Column(String(250))
 
     def __repr__(self):
-        return "<User(name='%s', fullname='%s', nickname='%s')>" % (self.name, self.fullname, self.nickname)
+        """
+        returns the string representation of the class.
+        """
+        return "<User(name='%s', fullname='%s', nickname='%s')>"
+                % (self.name, self.fullname, self.nickname)
